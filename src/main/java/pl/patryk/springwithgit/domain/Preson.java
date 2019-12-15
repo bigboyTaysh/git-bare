ed8b19bddf480c60e4d6df00eb5e3d86bd781964
bigboyTaysh <pvtryk.w@gmail.com>
2019-12-15 12:00:06 +0100

    [changes] - add controller

---
+++ src/main/java/pl/patryk/springwithgit/domain/Preson.java
@@ -0,0 +1,12 @@
+package pl.patryk.springwithgit.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Preson {
+    private String name;
+    private String surname;
+
+}

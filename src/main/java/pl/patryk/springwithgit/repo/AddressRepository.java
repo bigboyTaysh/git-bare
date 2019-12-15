697add540093619d1b4fcc873214131837fa3d47
bigboyTaysh <pvtryk.w@gmail.com>
2019-12-15 12:32:16 +0100

    [change] - add method in repo

--- src/main/java/pl/patryk/springwithgit/repo/AddressRepository.java
+++ src/main/java/pl/patryk/springwithgit/repo/AddressRepository.java
@@ -3,6 +3,8 @@ package pl.patryk.springwithgit.repo;
 import org.springframework.data.repository.CrudRepository;
 import pl.patryk.springwithgit.domain.Address;
 
-public interface AddressRepository extends CrudRepository<Address, Long> {
+import java.util.List;
 
+public interface AddressRepository extends CrudRepository<Address, Long> {
+    List<Address> findAllOrOrderByCity();
 }

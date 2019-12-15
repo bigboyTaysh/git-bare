1c1480d0ad362182fb5c0a2b1bd73f5869f25ac4
bigboyTaysh <pvtryk.w@gmail.com>
2019-12-15 10:33:21 +0100

    [change] - changes in controller

--- src/main/java/pl/patryk/springwithgit/controller/UserController.java
+++ src/main/java/pl/patryk/springwithgit/controller/UserController.java
@@ -1,8 +1,12 @@
 package pl.patryk.springwithgit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
 }
